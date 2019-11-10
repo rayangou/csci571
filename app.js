84ca233b508dca47d11023cfef2fe5972628372e
rayangou@usc.edu <rayangou@usc.edu>
2019-11-10 15:06:15 -0800

    adding getcstateseal

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
+const request = require("request");
 
 var app = express();
 
@@ -61,6 +62,50 @@ app.use(function(err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.render('error');
+});
+
+app.get('/getstateseal', (req, res) => {
+    state = req.query["state"];
+    url = 'https://www.googleapis.com/customsearch/v1?q=' + state + '%20State%20Seal&cx=016065621806241521131:a3cj5qp1rsm&imgSize=huge&imgType=news&num=1&searchType=image&key=AIzaSyAYVhtF-bEJCTQcGuay8p_QQ4pLNXFFWmM';
+
+    request.get(url,
+        (errorResponse, response, data) => {
+            res.setHeader("Access-Control-Allow-Origin", "*");
+            res.setHeader("Cache-Control", "no-cache");
+
+            if (errorResponse) {
+                res.status(404).send("Error while fetching images from google.");
+                return null;
+            }
+            data = JSON.parse(data);
+
+            if (!data ||
+                data === null
+            ) {
+                res.status(404).send("Error while fetching images from google.");
+                return null;
+            }
+
+            if (!data.hasOwnProperty("items") ||
+                data.items.length <= 0
+            ) {
+                res.status(404).send("No Records.");
+                return null;
+            }
+
+            let result = [];
+
+            let items = data.items;
+            for (let i = 0; i < items.length; i++) {
+                let item = items[i];
+
+                if (item.hasOwnProperty("link") && item.link.length > 0) {
+                    result.push(item.link);
+                }
+            }
+
+            res.send(result);
+        });
 });
 
 module.exports = app;
